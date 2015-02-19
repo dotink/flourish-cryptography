98697ee5d73f67bf71a4365b3a6256b160f898f8
Matthew J. Sahagian <msahagian@dotink.org>
2015-02-18 21:08:54 -0800

    Copyright and doc updates

--- src/Cryptography.php
+++ src/Cryptography.php
@@ -3,13 +3,13 @@
 	/**
 	 * Provides session-based messaging for page-to-page communication
 	 *
-	 * @copyright  Copyright (c) 2007-2012 Will Bond, others
-	 * @author     Will Bond           [wb]  <will@flourishlib.com>
-	 * @author     Matthew J. Sahagian [mjs] <msahagian@dotink.org>
+	 * @copyright Copyright (c) 2007-2015 Will Bond, Matthew J. Sahagian, others
+	 * @author Will Bond [wb] <will@flourishlib.com>
+	 * @author Matthew J. Sahagian [mjs] <msahagian@dotink.org>
 	 *
-	 * @license    Please reference the LICENSE.md file at the root of this distribution
+	 * @license Please reference the LICENSE.md file at the root of this distribution
 	 *
-	 * @package    Flourish
+	 * @package Flourish
 	 */
 	class Cryptography
 	{
